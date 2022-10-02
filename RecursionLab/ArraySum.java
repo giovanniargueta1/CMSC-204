a263f253c9666fa962029f8af5268ca508f19418
giovanniargueta1 <99226148+giovanniargueta1@users.noreply.github.com>
2022-10-02 19:07:05 -0400

    Add files via upload

---
+++ RecursionLab/ArraySum.java
@@ -0,0 +1,12 @@
+public class ArraySum {
+	public ArraySum() {
+		
+	}
+	
+	public int sumOfArray(Integer[] a, int index) {
+		if(index==0) {
+			return a[0];
+		}
+		return a[index] + sumOfArray(a, --index);
+	}
+}	
